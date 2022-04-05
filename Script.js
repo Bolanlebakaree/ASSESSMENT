bb1d890605e1e34dfa1215678419b6efa11ce255
Omobee <bolanle.bakare@gmail.com>
2022-04-05 18:31:20 -0400

    first commit

---
+++ Script.js
@@ -0,0 +1,16 @@
+let EvenGroup =[];
+let OddGroup =[];
+for (let i = 1; i <= 100; i++){
+        if (i % 2 === 0){
+            EvenGroup.push(i);
+        }
+        else{
+            OddGroup.push(i);
+        }
+    }
+
+console.log(EvenGroup.length + " is even");  
+console.log(OddGroup.length + " is odd");
+
+
+
